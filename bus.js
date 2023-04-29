27aab9b654d36f349735cca0134e57abe8d5cc90
Taras Strypko <esteewhy@hotmail.com>
2023-04-29 22:14:56 +0300

    + opposite side + front/rear view

--- bus.js
+++ bus.js
@@ -22,33 +22,28 @@ $(function() {
     
     function slot$(parentId, n) {
         var groupName = parentId + 'slot' + n;
+        var _grp = function(num, typ, tgt) {
+            return $.map(Array.from(new Array(num).keys()), function(n) {
+                return option$(typ, n, groupName, tgt);
+            });
+        };
         return $('<span class="slot" />').append(
             shadow$(groupName, 'open'),
             shadow$(groupName, 'close'),
             $('<div/>', {'class': 'picker'}).append(
                 option$('spot', 2, groupName, 'top', 'checked'),
-                $.map(Array.from(new Array(2).keys()), function(n) {
-                    return option$('t', n, groupName, 'top');
-                }),
-                $.map(Array.from(new Array(2).keys()), function(n) {
-                    return option$('h', n, groupName, 'top');
-                }),
+                _grp(2, 't', 'top'),
+                _grp(2, 'h', 'top'),
+                _grp(3, 'f', 'top'),
+                _grp(2, 'r', 'top'),
                 '<br/>',
-                $.map(Array.from(new Array(9).keys()), function(n) {
-                    return option$('d', n, groupName, 'top');
-                }),
+                _grp(9, 'd', 'top'),
                 '<br/>',
-                $.map(Array.from(new Array(9).keys()), function(n) {
-                    return option$('w', n, groupName, 'top');
-                }),
+                _grp(9, 'w', 'top'),
                 '<br/>',
-                $.map(Array.from(new Array(6).keys()), function(n) {
-                    return option$('a', n, groupName, 'bottom');
-                }),
+                _grp(6, 'a', 'bottom'),
                 '<br/>',
-                $.map(Array.from(new Array(6).keys()), function(n) {
-                    return option$('l', n, groupName , 'bottom');
-                })
+                _grp(6, 'l', 'bottom')
             )
         );
     }
@@ -57,8 +52,7 @@ $(function() {
         return $('<div class="bus" id="' + id + '" />')
             .append($.map(Array.from(new Array(20).keys()), function(n) {
                 return slot$(id, n);
-            }))
-            .append('<a class="share" href>share</a>');
+            }));
     }
     
     $('#slots').replaceWith(
@@ -93,6 +87,24 @@ $(function() {
         });
     }
 
+    inflateBus('#bus0', 't0 d7 w2 a0 w2 l0 d0 w2 l3 w2 a3 d5 h0 f0 h0 w3 l2 w2 a3 w2 l2 w2 w2 w2 a0 w2 l2 t0 r0');
+    inflateBus('#bus1', 't0 w4 l4 d0 w7 a1 w7 l1 w7 l1 w7 a4 d0 h0');
+    inflateBus('#bus2', 't0 w4 l4 w4 w8 a1 w8 l1 w8 l1 w8 a4 d0 h0');
+    inflateBus('#bus3', 't0 w6 d0 w8 a2 w8 l1 w2 a5 d0 h0');
+    inflateBus('#bus4', 't0 w4 l4 d0 w8 a1 w8 l1 w8 l1 w8 a4 d0 h0');
+    inflateBus('#bus5', 't0 d5 w2 a3 w2 l2 d5 t1 w2 a0 w2 l0 d5 w2 l3 w2 a3 d5 h0');
+    inflateBus('#bus6', 't0 w6 d6 w2 a0 w2 l0 d6 w0 l3 w2 a3 w6 d6 h0');
+    
+    $.each((function () {//https://stackoverflow.com/a/21152762/35438
+      return location.search
+        ? location.search.substr(1).split`&`.reduce((qd, item) => {let [k,v] = item.split`=`; v = v && decodeURIComponent(v); (qd[k] = qd[k] || []).push(v); return qd}, {})
+        : {}
+    })(), function(key, values) {
+        inflateBus('#' + key, values[0]);
+    });
+});
+
+$(function() {
     function deflateBus(board) {
         var cbnames = [];
         
@@ -108,26 +120,11 @@ $(function() {
         }).join(' ');
     }
     
-    inflateBus('#bus0', 't0d7 w2 a0 w2 l0 d7 w2 l3 w2 a3 d4 h0');
-    inflateBus('#bus1', 't0 w4 l4 d0 w7 a1 w7 l1 w7 l1 w7 a4 d0 h0');
-    inflateBus('#bus2', 't0 w4 l4 w4 w8 a1 w8 l1 w8 l1 w8 a4 d0 h0');
-    inflateBus('#bus3', 't0 w6 d0 w8 a2 w8 l1 w2 a5 d0 h0');
-    inflateBus('#bus4', 't0 w4 l4 d0 w8 a1 w8 l1 w8 l1 w8 a4 d0 h0');
-    inflateBus('#bus5', 't0 d5 w2 a3 w2 l2 d5 t1 w2 a0 w2 l0 d5 w2 l3 w2 a3 d5 h0');
-    inflateBus('#bus6', 't0 w6 d6 w2 a0 w2 l0 d6 w0 l3 w2 a3 w6 d6 h0');
+    $('.bus:not(:has(.share))').append('<a class="share" href>share</a>');
     
-    $(document).on('change', '[type="radio"]', function() {
+    $(document).on('change', '.bus [type="radio"]', function() {
         var board = $(this).parents('.bus');
         $('.share', board).attr('href', '?' + board.get(0).id + '=' + encodeURIComponent(deflateBus(board).replace(/\s/g, '')));
-    });
-    
-    $.each((function () {//https://stackoverflow.com/a/21152762/35438
-      return location.search
-        ? location.search.substr(1).split`&`.reduce((qd, item) => {let [k,v] = item.split`=`; v = v && decodeURIComponent(v); (qd[k] = qd[k] || []).push(v); return qd}, {})
-        : {}
-    })(), function(key, values) {
-        console.log([key, values[0]]);
-        inflateBus('#' + key, values[0]);
     });
     
     $('.bus .slot:first-of-type :input.open').trigger('change');
